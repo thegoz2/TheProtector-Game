a2e07175e41e750a745782197eb1a6b822ba6c0d
phupa pookphanich <56261767+thegoz2@users.noreply.github.com>
2020-12-05 11:46:19 +0700

    created model
    -score

---
+++ Game/src/Model.java
@@ -0,0 +1,15 @@
+public class Model {
+    private int Score;
+    public Model(){
+
+    }
+
+    public int getScore() {
+        return Score;
+    }
+
+    public void setScore(int score) {
+        Score = score;
+    }
+}
+
